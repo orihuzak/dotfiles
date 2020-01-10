d5f7274b175e11742120170752020cfe547463e4
orihuzak <arx.solid@gmail.com>
2020-01-11 00:44:23 +0900

    add tom4a.sh, mp4から音声(m4a)を取り出すscript

---
+++ tom4a.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# ffmpegでmp4から音声ファイルのm4aを取り出すshell script
+# 使い方
+#   path/to/tom4a.sh 変換したい動画ファイル
+# output
+#   拡張子以外の名前の部分はinput fileと同じ
+#   拡張子はm4aになる
+input="$1"
+output=${input%.*}'.m4a'
+ffmpeg -i "${input}" -vn -acodec copy "${output}"
+exit 0
