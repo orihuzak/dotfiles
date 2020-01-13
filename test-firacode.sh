d40bdf9950b72920415a30fdb32ae9508b7148a1
orihuzak <arx.solid@gmail.com>
2020-01-14 01:38:06 +0900

    firacodeインストール用のtest scriptを用意した

--- test-firacode.sh
+++ test-firacode.sh
@@ -1,15 +1,30 @@
 #!/bin/bash
+# A shell script to install Firacode font manually
+# Firacode
+#   https://github.com/tonsky/FiraCode
+# How to install fonts for debian
+#   https://wiki.debian.org/Fonts
 
-# curl -L raw.githubusercontent.com/tonsky/FiraCode/master/distr/ttf/FiraCode-Bold.ttf | echo
+fonts_dir="usr/local/share/fonts"
+if [ ! -d "${fonts_dir}" ]; then
+    echo "mkdir -p $fonts_dir"
+    mkdir -p "${fonts_dir}"
+else
+    echo "Found fonts dir $fonts_dir"
+fi
+
 # see https://wiki.debian.org/Fonts
 for type in Bold Light Medium Regular Retina; do
     # for all usersのディレクトリに保存
     file_path="/usr/local/share/fonts/FiraCode-${type}.ttf"
     file_url="raw.githubusercontent.com/tonsky/FiraCode/master/distr/ttf/FiraCode-${type}.ttf"
     if [ ! -e "${file_path}" ]; then
-        echo "wget -O $file_path $file_url"
-        wget -O "${file_path}" "${file_url}"
+      echo "curl -o $file_path $file_url"
+      curl -o "$file_path" "$file_url"
     else
-  echo "Found existing file $file_path"
+      echo "Found existing file $file_path"
     fi;
 done
+
+echo "fc-cache -f"
+fc-cache -f
