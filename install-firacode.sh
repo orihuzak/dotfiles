bdac72da6f15a0978241c177c8082fe4bcdfa9ce
orihuzak <arx.solid@gmail.com>
2020-01-16 18:11:51 +0900

    fix install-firacode.sh. firacode can now be used

--- install-firacode.sh
+++ install-firacode.sh
@@ -6,28 +6,12 @@
 #   https://github.com/tonsky/FiraCode
 # How to install fonts for debian
 #   https://wiki.debian.org/Fonts
+# firacode_2.zip
+# https://github.com/tonsky/FiraCode/releases/download/2/FiraCode_2.zip
 
-fonts_dir="/usr/local/share/fonts"
-if [ ! -d "${fonts_dir}" ]; then
-    echo "mkdir -p $fonts_dir"
-    # エラーを表示せず記述したディレクトリが存在しなければ作成する
-    mkdir -p "${fonts_dir}"
-else
-    echo "Found fonts dir $fonts_dir"
-fi
-
-# see https://wiki.debian.org/Fonts
-for type in Bold Light Medium Regular Retina; do
-  # for all usersのディレクトリに保存
-  file_path="/usr/local/share/fonts/FiraCode-${type}.ttf"
-  file_url="raw.githubusercontent.com/tonsky/FiraCode/master/distr/ttf/FiraCode-${type}.ttf"
-  if [ ! -e "${file_path}" ]; then
-    echo "sudo curl -L $file_url -o $file_path"
-    sudo curl -L "$file_url" -o "$file_path"
-  else
-    echo "Found existing file $file_path"
-  fi
-done
-
-echo "fc-cache -f"
-fc-cache -f
+curl -OL https://github.com/tonsky/FiraCode/releases/download/2/FiraCode_2.zip
+unzip FiraCode_2.zip -d FiraCode_2
+sudo cp ./FiraCode_2/otf/* /usr/local/share/fonts
+sudo fc-cache -fv
+# if you want to remove  FiraCode_2.zip and ./FiraCode_2 directory, remove comment out below
+# sudo rm -r ./FiraCode_2.zip ./FiraCode_2
