c98c7e00b2c7599ad44f00db975624537a8bc979
orihuzak <arx.solid@gmail.com>
2020-01-26 06:17:09 +0900

    add a function to installing pip

--- setup-crostini.sh
+++ setup-crostini.sh
@@ -118,6 +118,13 @@ getTrashCLI() {
   sudo python setup.py install
 }
 
+getPip() {
+  # intall pip
+  # see https://pip.pypa.io/en/stable/installing/
+  curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+  python get-pip.py
+}
+
 
 sudo apt-get update
 # repositoryの追加
