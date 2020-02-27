204d87de9a91e21d60736764862a6d0756ff0a75
orihuzak <arx.solid@gmail.com>
2020-02-28 01:56:51 +0900

    move .hyper.js to .config/Hyper/

---
+++ .config/Hyper/.hyper.js
@@ -0,0 +1,27 @@
+// hyper terminal setting file
+
+module.exports = {
+  "config": {
+    fontSize: 11,
+    // フォントの設定
+    fontFamily: 'monospace, "DroidSansMono Nerd Font"',
+    // 透明度の設定
+    "opacity": 0.85,
+    // 選択範囲をコピーする
+    "copyOnSelect": true,
+    "cursorBlink": true,
+    "cursorShape": "BLOCK",
+  },
+  "plugins": [
+    "hyper-statusline",
+    "hyper-search",
+    "hyper-opacity",
+    "hyper-pane",
+    "hyper-tab-icons-plus"
+  ],
+  "keymaps": {
+    "editor:cut": "ctrl+x",
+    "editor:copy": "ctrl+c",
+    "editor:paste": "ctrl+v",
+  }
+};
