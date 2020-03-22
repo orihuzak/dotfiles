c0c74f59bdfe288db9963c50009e7844d5583403
orihuzak <arx.solid@gmail.com>
2020-03-22 12:01:09 +0900

    rename symlink-from-gdrive.sh to symlinks-for-chromeos.sh

---
+++ symlinks-for-chromeos.sh
@@ -0,0 +1,7 @@
+# gdriveからのsimlinks
+
+ln -s /mnt/chromeos/GoogleDrive/MyDrive/koe/reina/ ~/koe/reina
+ln -s /mnt/chromeos/GoogleDrive/MyDrive/koe/rin/ ~/koe/rin
+ln -s /mnt/chromeos/GoogleDrive/MyDrive/koe/moa/ ~/koe/moa
+ln -s /mnt/chromeos/MyFiles/game/ ~/game
+
