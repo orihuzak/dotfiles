5a57ea755fb5b87fea09bbe2e2577ab7b7c714ec
orihuzak <arx.solid@gmail.com>
2020-03-22 11:43:43 +0900

    add file to symlink from google drive

---
+++ symlink-from-gdrive.sh
@@ -0,0 +1,6 @@
+# gdriveからのsimlinks
+
+ln -s /mnt/chromeos/GoogleDrive/MyDrive/koe/reina/ ~/koe/reina
+ln -s /mnt/chromeos/GoogleDrive/MyDrive/koe/rin/ ~/koe/rin
+ln -s /mnt/chromeos/GoogleDrive/MyDrive/koe/moa/ ~/koe/moa
+
