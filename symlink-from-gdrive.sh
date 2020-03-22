d0e70273d36ab590bce2788191fe838c15200921
orihuzak <arx.solid@gmail.com>
2020-03-22 12:01:57 +0900

    delete symlink-from-gdrive.sh

--- symlink-from-gdrive.sh
+++
@@ -1,6 +0,0 @@
-# gdriveからのsimlinks
-
-ln -s /mnt/chromeos/GoogleDrive/MyDrive/koe/reina/ ~/koe/reina
-ln -s /mnt/chromeos/GoogleDrive/MyDrive/koe/rin/ ~/koe/rin
-ln -s /mnt/chromeos/GoogleDrive/MyDrive/koe/moa/ ~/koe/moa
-
