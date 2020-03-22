0e8b3236768519f3e8ae103a2afad23577598fb8
orihuzak <arx.solid@gmail.com>
2020-03-22 12:11:19 +0900

    [symlinks-for-chromeos.sh] add symlink Brewfile

--- symlinks-for-chromeos.sh
+++ symlinks-for-chromeos.sh
@@ -4,4 +4,5 @@ ln -s /mnt/chromeos/GoogleDrive/MyDrive/koe/reina/ ~/koe/reina
 ln -s /mnt/chromeos/GoogleDrive/MyDrive/koe/rin/ ~/koe/rin
 ln -s /mnt/chromeos/GoogleDrive/MyDrive/koe/moa/ ~/koe/moa
 ln -s /mnt/chromeos/MyFiles/game/ ~/game
+ln -s ~/dotfiles/Brewfile ~/Brewfile
 
