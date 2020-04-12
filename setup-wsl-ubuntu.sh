c0e9753f9f9bec96ec4c640fc32f8a672b5da914
rakaz <arx.solid@gmail.com>
2020-04-12 18:58:52 +0900

    add set-wsl-ubuntu

---
+++ setup-wsl-ubuntu.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# setup wsl2 Ubuntu
+# clone dotfiles from github
+# symlink dotfiles to pertinent directory
+
+DOTFILES=~/dotfiles
+GITHUB_URL=https://github.com/orihuzak/dotfiles.git
+
+sudo apt-get update
+
+# symlink
+
