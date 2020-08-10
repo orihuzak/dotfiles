03827258fa3cf38d560d54c9a1e2808ecc87a18c
Kazuhiro Haraguchi <arx.solid@gmail.com>
2020-08-11 03:25:01 +0900

    [Ruby] add install bundler script

--- etc/init.sh
+++ etc/init.sh
@@ -18,6 +18,9 @@ pip3 install pynvim
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
   https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
+## for ruby
+gem install bundler
+
 # install deno
 curl -fsSL https://deno.land/x/install/install.sh | sh
 
