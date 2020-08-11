11d283fce1f7dfe2ab81a4256c8ff8c5a811c80a
Kazuhiro Haraguchi <arx.solid@gmail.com>
2020-08-11 19:40:42 +0900

    [dotfiles] display descriptions of what init.sh is doing

--- etc/init.sh
+++ etc/init.sh
@@ -2,26 +2,32 @@
 
 ## install packages to build environment
 
+echo -e "\nsudo apt-get update"
 sudo apt-get update
 
 # install Homebrew
-if type brew >/dev/null 2>&1; then
-  brew bundle
-else
+if ! type brew >/dev/null 2>&1; then
+  echo -e "\nInstall Homebrew"
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
-  brew bundle
 fi
 
+echo -e "\nInstall Homebrew formulae"
+brew bundle
+
 ## for nvim
+echo -e "\nInstall pynvim"
 pip3 install pynvim
 # install vim-plug
+echo -e "\nInstall vim-plug"
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
   https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
 ## for ruby
+echo -e "\nInstall bundler, a ruby gem"
 gem install bundler
 
 # install deno
+echo -e "\nInstall deno"
 curl -fsSL https://deno.land/x/install/install.sh | sh
 
 
