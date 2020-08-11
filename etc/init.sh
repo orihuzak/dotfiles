a288a1dab479e24bcb95400f419d9f2673238250
Kazuhiro Haraguchi <arx.solid@gmail.com>
2020-08-11 22:33:57 +0900

    [dotfiles] move reload .bash_profile scirpt to file top

--- etc/init.sh
+++ etc/init.sh
@@ -2,9 +2,6 @@
 
 ## install packages to build environment
 
-echo -e "\nReload .bash_profile"
-. $HOME/.bash_profile
-
 echo -e "\nsudo apt-get update"
 sudo apt-get update
 
@@ -14,8 +11,14 @@ if ! type brew >/dev/null 2>&1; then
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 fi
 
+echo -e "\nReload .bash_profile"
+. $HOME/.bash_profile
+
 echo -e "\nInstall Homebrew formulae"
 brew bundle
+
+echo -e "\nReload .bash_profile"
+. $HOME/.bash_profile
 
 ## for nvim
 echo -e "\nInstall pynvim"
@@ -24,6 +27,9 @@ pip3 install pynvim
 echo -e "\nInstall vim-plug"
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
   https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+
+echo -e "\nReload .bash_profile"
+. $HOME/.bash_profile
 
 echo -e "\nInstall n/vim plugins"
 nvim +PlugInstall +qall
